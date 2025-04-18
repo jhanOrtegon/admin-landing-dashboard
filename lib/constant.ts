3cc1c0bb37232e9a260079142875d69aebee53a3
Jhan carlos ortegon <jhancarlos.ortegon@gmail.com>
2025-04-17 19:21:37 -0500

    new progress

--- lib/constant.ts
+++ lib/constant.ts
@@ -1,9 +1,9 @@
 import { v4 as uuid } from 'uuid';
 
-export const BASE_URL = process.env.API_BASE_URL;
+export const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-export const USER_LOGIN = process.env.USER_LOGIN;
+export const USER_LOGIN = process.env.NEXT_PUBLIC_USER_LOGIN;
 
-export const PASSWORD_LOGIN = process.env.PASSWORD_LOGIN;
+export const PASSWORD_LOGIN = process.env.NEXT_PUBLIC_PASSWORD_LOGIN;
 
 export const randomId = () => uuid();
