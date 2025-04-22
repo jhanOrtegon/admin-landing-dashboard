26b15657cf055cd094fa44d4499cb0f0391db4c5
desarrolladortic4 <desarrolladortic4@ttncompany.com>
2025-04-22 17:04:48 -0500

    feat new progress

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -18,10 +18,10 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
         )}
         <textarea
           id={id}
-          maxLength={150}
+          maxLength={350}
           placeholder="Escribir..."
           className={cn(
-            'flex w-full rounded-md border border-input bg-white dark:bg-zinc-900 text-gray-900 dark:text-gray-100 px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm min-h-[10px] max-h-[100px]',
+            'flex w-full rounded-md border border-input bg-white dark:bg-zinc-900 text-gray-900 dark:text-gray-100 px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm min-h-[10px] max-h-[150px]',
             className
           )}
           ref={ref}
