d924060122546c0b472f6baa50e7a7b9d4f5e086
desarrolladortic4 <desarrolladortic4@ttncompany.com>
2025-04-23 17:41:03 -0500

    feat: new progress

--- app/dashboard/vacantes/page.tsx
+++ app/dashboard/vacantes/page.tsx
@@ -3,14 +3,14 @@ import VacantesClientPage from './_components/vacantes-client-page';
 import { getVacantes } from './_actions/get-vacancie';
 import { getOpciones, TOpcion } from '../../../selects/options';
 
-export default async function VacantesPage({
-  searchParams
-}: {
-  searchParams: { q: string; offset: string };
+export default async function VacantesPage(props: {
+  searchParams: Promise<{ q: string; offset: string }>;
 }) {
+  const searchParams = await props.searchParams;
   const search = searchParams.q ?? '';
   const offset = Number(searchParams.offset ?? 0);
-  const { vacantes } = await getVacantes(search, offset, 10);
+
+  const { vacantes } = await getVacantes(search, offset, 100);
   const tecnologias = await getOpciones<TOpcion>('tecnologias');
 
   return (
